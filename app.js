8fc52daff1832ce9067738ba158d1e1ad0ce5b2d
[Mandi Solone] <[mandisolone@gmail.com]>
2024-07-15 19:10:31 -0400

    added fetch API from GIPHY

--- app.js
+++ app.js
@@ -2,3 +2,35 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let searchBtn = document.querySelector("#submitSearch"); 
+let input = document.querySelector("#searchWord"); 
+let imageEl = document.querySelector("#image1");
+let feedBackP = document.querySelector("#feedBackPara");
+let form = document.querySelector("#form1");
+
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+
+const GIPHY_KEY = "FAesj2nRckKxKWaoPy39C7RopRKdsIiY";
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    fetch(`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${input.value}`)
+    .then((res) => {
+        return res.json();
+    })
+    .then((body) => {
+        console.log(body);
+        imageEl.src = body.data.images.original.url; 
+        imageEl.alt = body.data.title; 
+        feedBackP.textContent = input.value;
+    })
+    .catch((err) => {
+        console.error(err);
+        feedBackP.textContent = err.message;
+    })
+    .finally(() => {
+        input.value = ""; 
+    });
+});
